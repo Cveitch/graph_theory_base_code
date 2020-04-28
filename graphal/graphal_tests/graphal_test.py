72aa1c37979fdb90632d4aa556cbd05989f7275e
conan <conanveitch@gmail.com>
2020-04-27 22:46:43 -0700

    Graphlets can now be generated/counted.

--- graphal/graphal_tests/graphal_test.py
+++ graphal/graphal_tests/graphal_test.py
@@ -8,6 +8,7 @@ import graphal.graff_gen.CSVGraffReader as csvgr
 import graphal.graff_gen.UndirectedGraff as ugraff
 import matplotlib.pyplot as plt
 import networkx as nx
+import graphal.graphlets.GraphletCounter as graphlet
 
 graf = ugraff.UndirectedGraff()
 
@@ -17,11 +18,17 @@ graf = ugraff.UndirectedGraff()
 # cgr = csvgr.CSVGraffReader('edge_list_csv.csv')
 # graf.build_from_edge_list(cgr.csv_to_edge_list())
 
-# cgr = csvgr.CSVGraffReader('adj_list_csv.csv')
-# graf.build_from_adj_list(cgr.csv_to_adj_list())
+cgr = csvgr.CSVGraffReader('adj_list_csv.csv')
+graf.build_from_adj_list(cgr.csv_to_adj_list())
 
-cgr = csvgr.CSVGraffReader('adj_mat_csv.csv')
-graf.build_from_adj_matrix(cgr.csv_to_adj_matrix())
+#cgr = csvgr.CSVGraffReader('adj_mat_csv.csv')
+#graf.build_from_adj_matrix(cgr.csv_to_adj_matrix())
+
+gcounter = graphlet.GraphletCounter(graf.G)
+print(gcounter.count_n_graphlets(5))
+
+
+
 
 nx.draw(graf.G, node_size=500, with_labels=True)
 plt.show()
