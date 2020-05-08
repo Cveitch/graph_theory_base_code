60e17a06338c779d69731c1a20b4cfa5df8f8d6b
conan <conanveitch@gmail.com>
2020-05-08 02:36:14 -0700

    Implemented Local Complexity Measure and Shannon Diversity Measure. Rewrote graphal_test.py for clarity.

--- graffal/complexity/ComplexityMeasurer.py
+++ graffal/complexity/ComplexityMeasurer.py
@@ -3,28 +3,57 @@
 #
 # Description: 
 #
+import math
+from collections import Counter
 
-class ComplexityMeasurer:
-    def __init__(self, graff):
-        self.g = graff
 
-    def vertex_degree_info_measure(self):
-        return 0
+def vertex_degree_info_measure(self):
+    return 0
 
-    def complexity_B(self):
-        return 0
 
-    def graph_distance_complexity(self):
-        return 0
+def complexity_b(self):
+    return 0
 
-    def total_walk_count(self):
-        return 0
 
-    def topological_info_complexity(self):
-        return 0
+def graph_distance_complexity(self):
+    return 0
 
-    def shannon_diversity_measure(self):
-        return 0
 
-    def local_complexity_measure(self):
-        return 0
+def total_walk_count(self):
+    return 0
+
+
+def topological_info_complexity(self):
+    return 0
+
+
+def shannon_diversity_measure(freq):
+    total_freq = 0
+    for nodes in freq:
+        total_freq += freq[nodes]
+    h_gam = 0
+    for nodes in freq:
+        if freq[nodes] != 0:
+            freq_prop = freq[nodes] / total_freq
+            h_gam += freq_prop * math.log2(freq_prop)
+    return (-1) * h_gam
+
+
+def local_complexity_measure(orbits):
+    h_gam = {}
+    orbit_counts = {}
+    for i in range(30):
+        for nodes in orbits:
+            if i not in orbit_counts:
+                orbit_counts[i] = orbits[nodes].count(i)
+            else:
+                orbit_counts[i] += orbits[nodes].count(i)
+    for nodes in orbits:
+        h_gam[nodes] = 0
+        for i in range(30):
+            if i in orbits[nodes]:
+                freq_orb = orbits[nodes].count(i) / orbit_counts[i]
+                h_gam[nodes] += freq_orb * math.log2(freq_orb)
+    for nodes in orbits:
+        h_gam[nodes] *= (-1)
+    return h_gam
--- graffal/graffal_tests/graphal_test.py
+++ graffal/graffal_tests/graphal_test.py
@@ -4,45 +4,52 @@
 # Description: Test cases for graffal project.
 #
 
-
 import graffal.graff_gen.GraffWrapper as gmake
 import graffal.grafflet_gen.GraffletWrapper as gwrap
-import graffal.grafflet_gen.GraffletCounter as gcounter
+import graffal.complexity.ComplexityMeasurer as comeas
+
 
 cgr = "test_edge.csv"
 G = gmake.GraffWrapper(cgr)
 gwrapper = gwrap.GraphletWrapper(G.get_graff())
 
-# n = 4
-# gcount = gcounter.GraffletCounter(G.get_graff(), n)
-#
-# print(*gcount.grafflet_edges, sep="\n")
-# print("There are:", gcount.grafflet_count, "total", n, "node graphlets.")
-#
+
+def print_edgelist():
+    print("Graphlets edge list:")
+    print(*gwrapper.get_grafflet_edges(), sep="\n")
 
 
+def print_total_graphlets():
+    for i in range(2, 6):
+        print("There are:", gwrapper.grafflet_count[i], "total", i, "node graphlets.")
 
 
-print("Graphlets edge list:")
-print(*gwrapper.get_grafflet_edges(), sep="\n")
+def print_graphlet_frequency():
+    for node, freq in gwrapper.get_grafflet_freq().items():
+        print("Graphlet", node, "has frequency:", freq)
 
-for i in range(2, 6):
-    print("There are:", gwrapper.grafflet_count[i], "total", i, "node graphlets.")
 
-for node, freq in gwrapper.get_grafflet_freq().items():
-    print("Graphlet", node, "has frequency:", freq)
+def print_node_orbits():
+    for node, orb in gwrapper.get_orbit_counts().items():
+        print("Node", node, "has orbits:", orb)
 
-for node, orb in gwrapper.get_orbit_counts().items():
-    print("Node", node, "has orbits:", orb)
+
+def print_shannon_diversity_measure():
+    sdm = comeas.shannon_diversity_measure(gwrapper.get_grafflet_freq())
+    print("Shannon Diversity Measure is: ", sdm)
+
+
+def print_local_complexity_measure():
+    lcm = comeas.local_complexity_measure(gwrapper.get_orbit_counts())
+    for node in lcm:
+        print("Local Complexity Measure of node", node, "is:", lcm[node])
+
+
+#print_edgelist()
+print_node_orbits()
+print_total_graphlets()
+#print_graphlet_frequency()
+print_shannon_diversity_measure()
+print_local_complexity_measure()
 
 G.draw_graff()
-
-#print(gcount.orbit_counts, sep="\n")
-
-#print(*gcount.orbit_counts, sep="\n")
-#print(graflet.get_node_orbit(4))
-
-#This is how we get nodes from graffs
-#print(list(graf.G.nodes())[8])
-
-#graf.G.degree[0]
