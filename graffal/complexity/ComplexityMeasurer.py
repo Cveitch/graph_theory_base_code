b95e8b6a68e28054ce21969f8598f3c83d5ccd92
conan <conanveitch@gmail.com>
2020-05-10 21:57:12 -0700

    Split shannon diversity measure into two sub measures. Cleaned up test file.

--- graffal/complexity/ComplexityMeasurer.py
+++ graffal/complexity/ComplexityMeasurer.py
@@ -1,21 +1,47 @@
 # ComplexityMeasurer.py
 # Conan Veitch, 2020-05-06
 #
-# Description: 
+# Description: Different complexity measures for graphs.
+#               NOTE: Both orbit functions are hard-coded with the value 30 (there are 29 graphlets)
+#                     Fix this later on.
 #
+
 import math
+import networkx as nx
+import itertools
 from collections import Counter
 
 
-def vertex_degree_info_measure(self):
-    return 0
+def vertex_degree_info_measure(graff):
+    # Order here doesn't matter, so we use a list for degrees.
+    degrees = [val for (node, val) in graff.degree()]
+    degree_val = 0
+    for ai in degrees:
+        if ai > 0:
+            degree_val += ai * math.log2(ai)
+    return degree_val
 
 
-def complexity_b(self):
-    return 0
+def complexity_b(graff):
+    b = 0
+    degrees = dict(graff.degree)
+    inter_node_d = dict(nx.all_pairs_shortest_path_length(graff))
+    d = {}
+    for nodes in inter_node_d:
+        for other_nodes in inter_node_d:
+            if nodes not in d:
+                d[nodes] = inter_node_d[nodes][other_nodes]
+            else:
+                d[nodes] += inter_node_d[nodes][other_nodes]
+    for nodes in inter_node_d:
+        ai = degrees[nodes]
+        di = d[nodes]
+        b += ai / di
+    return b
 
 
-def graph_distance_complexity(self):
+def graph_distance_complexity(graff, k):
+
     return 0
 
 
@@ -27,16 +53,37 @@ def topological_info_complexity(self):
     return 0
 
 
-def shannon_diversity_measure(freq):
+def frequency_info_sdm(freq):
     total_freq = 0
+    hgf = 0
     for nodes in freq:
         total_freq += freq[nodes]
-    h_gam = 0
     for nodes in freq:
-        if freq[nodes] != 0:
-            freq_prop = freq[nodes] / total_freq
-            h_gam += freq_prop * math.log2(freq_prop)
-    return (-1) * h_gam
+        if freq[nodes] != 0 and total_freq != 0:
+            p_i = freq[nodes] / total_freq
+            hgf += p_i * math.log2(p_i)
+    return (-1) * hgf
+
+
+def orbit_distribution_info_sdm(orbits):
+    total_freq = 0
+    hgd = 0
+    orbit_freq = {}
+    for i, j in orbits.items():
+        for orbs in j:
+            total_freq += 1
+    for i in range(30):
+        for j, k in orbits.items():
+            for orbs in k:
+                if orbs == i:
+                    if i not in orbit_freq:
+                        orbit_freq[i] = 1
+                    else:
+                        orbit_freq[i] += 1
+    for orb in orbit_freq:
+        p_i = orbit_freq[orb] / total_freq
+        hgd += p_i * math.log2(p_i)
+    return (-1) * hgd
 
 
 def local_complexity_measure(orbits):
